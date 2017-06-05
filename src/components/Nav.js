24dff6828a302c4a8cf01889420705ecef3dddc8
RicardoG2016 <gomez.r@live.com>
2017-06-05 14:09:11 -0500

    removed extra white space.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,37 +6,36 @@ import wrd from '../images/logos/wrd.png';
 class Nav extends Component {
   render() {
     return (
-      <div>
-            <div className="navbar navbar-default main-menu">
-                <div className="container nav-con">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <Link to="/home" className="navbar-brand">
-                            <img src={wrd} alt="logo" />
-                        </Link>
-                    </div>
-                    <div>
-                        <ul className="nav navbar-nav navbar-left">
-                            <li><Link to="/home">World Rainforest Day</Link></li>
-                        </ul>
-                    </div>
-                    <div className="collapse navbar-collapse">
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><Link to="/about">About</Link></li>
-                            <li><Link to="/news">News</Link></li>
-                            <li><Link to="/contact">Contact</Link></li>
-                            <li><span><a href="https://rainforestpartnership.org/partner-with-us/donation/"><button className="donate btn-lg btn-secondary btn">Donate</button></a></span></li>
-                        </ul>
-                    </div>
-                </div>
+        <div className="bs-component main-menu">
+          <nav className="navbar navbar-default">
+            <div className="container-fluid nav-con">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <Link to="/home" className="navbar-brand">
+                    <img src={wrd} alt="logo" />
+                </Link>
+                <span className="nav navbar-nav navbar-left"><Link className="logo-text" to="/home">World Rainforest Day</Link></span>
+              </div>
+
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav navbar-right">
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/news">News</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><span><a href="https://rainforestpartnership.org/partner-with-us/donation/"><button className="navdo donate btn-lg btn-secondary btn">Donate</button></a></span></li>
+                </ul>
+              </div>
             </div>
-      </div>
+          </nav>
+        </div>
     );
   }
 }
 
 export default Nav;
+
