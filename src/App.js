d504a30ac2e663aeb2c628cee1ef38eb6d5096e9
RicardoG2016 <gomez.r@live.com>
2017-06-14 00:52:33 -0500

    new build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Routes from './routes';
-import ScrollableAnchor from 'react-scrollable-anchor';
+
 
 export default class App extends React.Component {
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import ScrollableAnchor from 'react-scrollable-anchor';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
